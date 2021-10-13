32eac4ea352d4148e56614ad2089bf3625e4a4b2
Jeremy Daly <jeremydaly@users.noreply.github.com>
2021-10-13 16:59:55 -0400

    update vue-js

--- templates/vue-js/frontend/src/components/HelloWorld.vue
+++ templates/vue-js/frontend/src/components/HelloWorld.vue
@@ -19,6 +19,15 @@
         <span :class="user.value.status">{{ user.value.status }}</span>
       </div>
     </div>
+
+    <h3>Edit this Vue.js app locally:</h3>
+    <p>
+      Open a separate terminal, run <code>cd frontend</code> and then
+      <code>npm run serve</code>. You can then make changes and view them
+      locally. If you want to connect to your personal developer instance from
+      your local Vue project, update the <code>axios.defaults.baseURL</code> in
+      <code>/frontend/src/main.js</code> to your URL.
+    </p>
   </div>
 </template>
 
@@ -37,10 +46,16 @@ export default {
     };
   },
   created() {
-    axios.get("/api/users").then((response) => {
-      this.users = response.data.users;
-      this.loading = false;
-    });
+    axios
+      .get("/api/users")
+      .then((response) => {
+        this.users = response.data.users;
+        this.loading = false;
+      })
+      .catch((error) => {
+        console.log(error);
+        this.loading = false;
+      });
   },
 };
 </script>
@@ -50,7 +65,7 @@ h3 {
   margin: 40px 0 0;
 }
 a {
-  color: #42b983;
+  color: #fe5750;
 }
 
 #users {
@@ -61,6 +76,17 @@ a {
   color: #42b983;
 }
 .inactive {
-  color: #cc0000;
+  color: #fe5750;
+}
+
+p {
+  max-width: 600px;
+  margin: 1em auto;
+}
+
+code {
+  background: #f1f1f1;
+  padding: 0.2em;
+  color: #fe5750;
 }
 </style>
