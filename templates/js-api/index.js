2098f366cf6a74e2be9ebfcaf3bff5b615672526
Jeremy Daly <jeremydaly@users.noreply.github.com>
2021-10-13 09:12:22 -0400

    update js-api template

--- templates/js-api/index.js
+++ templates/js-api/index.js
@@ -1,20 +1,16 @@
-/*
-  The @serverless/cloud package is included by default in the cloud runtime.
-  So you don't have to include it in package.json.
-  
-  Use 'api' to build REST APIs, 'data' to access Serverless Data, and 'schedule'
-  to create scheduled tasks.
-
-  If you want to serve up static assets, just put them in the '/static' folder
-*/
 import { api, data, schedule, params } from "@serverless/cloud";
 
 // Create GET route and return users
 api.get("/users", async (req, res) => {
   // Get users from Serverless Data
-  let result = await data.get("users:*", true);
+  let result = await data.get("user:*", true);
   // Return the results
   res.send({
     users: result.items,
   });
+});
+
+// Redirect to users endpoint
+api.get("/*", (req, res) => {
+  res.redirect("/users");
 });
