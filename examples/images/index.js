ffb5708098a00ded9359f59ee01f2b3d2f070d5a
Eslam A. Hefnawy <eahefnawy@gmail.com>
2021-12-23 21:34:18 +0300

    add image hosting example app

---
+++ examples/images/index.js
@@ -0,0 +1,35 @@
+import { api, storage } from "@serverless/cloud";
+const random = (length = 6) => Math.random().toString(20).substr(2, length);
+
+api.upload("/", async (req, res) => {
+  const fileBuff = req.files[0].buffer;
+
+  if (!fileBuff) {
+    return res.status(400).send("No file provided");
+  }
+
+  try {
+    const imageId = random();
+    await storage.write(imageId, fileBuff);
+
+    return res.send({ url: `https://${req.hostname}/${imageId}` });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).send(error.message);
+  }
+});
+
+api.get("/:imageId", async (req, res) => {
+  const imageId = req.params.imageId;
+
+  try {
+    return await res.sendFile(imageId);
+  } catch (error) {
+    console.error(error);
+
+    let status = error.message === "NotFound" ? 400 : 500;
+
+    return res.status(status).send(error.message);
+  }
+});
