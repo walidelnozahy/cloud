87d37aa01505dcb86c205b73ffc7421c2e502cd7
Jeremy Daly <jeremydaly@users.noreply.github.com>
2022-01-27 17:24:37 -0500

    Update index.js

--- templates/js-api/index.js
+++ templates/js-api/index.js
@@ -1,16 +1,5 @@
-import { api, data } from "@serverless/cloud";
+import { api } from "@serverless/cloud";
 
-// Create GET route and return users
-api.get("/users", async (req, res) => {
-  // Get users from Serverless Data
-  let result = await data.get("user:*", true);
-  // Return the results
-  res.send({
-    users: result.items,
-  });
-});
-
-// Redirect to users endpoint
-api.get("/*", (req, res) => {
-  res.redirect("/users");
+api.get("/", async (req, res) => {
+  res.send("Hello Serverless Cloud!");
 });
